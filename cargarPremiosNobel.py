3a147309b48d55cac8ea32e24f36a1ef7360a220
diegoblanco3010 <dblanco@kyramadrid.es>
2024-11-28 13:11:07 +0100

    Update cargarPremiosNobel.py

--- cargarPremiosNobel.py
+++ cargarPremiosNobel.py
@@ -5,11 +5,9 @@ from io import StringIO
 import csv
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno
 load_dotenv()
 uri = os.getenv("MONGO_URI")
 
-# Conexión a MongoDB Atlas
 try:
     client = MongoClient(uri)
     print("Conexión exitosa a MongoDB Atlas.")
@@ -18,23 +16,18 @@ except Exception as e:
     exit()
 
 def cargarPremiosNobel():
-    # URL de la API del Premio Nobel
     url = "http://api.nobelprize.org/v1/prize.csv"
     
-    # Realizar la solicitud GET
     response = requests.get(url)
     
     if response.status_code == 200:
         csv_data = StringIO(response.text)
         reader = csv.DictReader(csv_data)
         
-        # Conexión a la base de datos y colección
         db = client["PremiosPrizes"]
         collection = db["NobelWinners"]
 
-        # Insertar datos en la colección
         for row in reader:
-            # Obtener los datos relevantes del archivo CSV
             year = row['year']
             category = row['category']
             nobel_id = row['id']
@@ -43,7 +36,6 @@ def cargarPremiosNobel():
             motivation = row.get('motivation', '').replace('"""', '').strip()
             share = row['share']
             
-            # Documento a insertar
             document = {
                 "year": year,
                 "category": category,
@@ -54,7 +46,6 @@ def cargarPremiosNobel():
                 "share": share
             }
             
-            # Insertar el documento si no existe
             if not collection.find_one({"year": year, "category": category, "id": nobel_id}):
                 collection.insert_one(document)
             else:
@@ -64,5 +55,4 @@ def cargarPremiosNobel():
     else:
         print(f"Error al obtener los datos de la API: {response.status_code}")
 
-# Llamar a la función para cargar los Premios Nobel
 cargarPremiosNobel()
