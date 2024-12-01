73ad225188d983eafa03b23c1aac337ba5cd681a
diegoblanco3010 <dblanco@kyramadrid.es>
2024-12-01 13:06:22 +0100

    Create cargarPremiosOscar.py

---
+++ cargarPremiosOscar.py
@@ -0,0 +1,43 @@
+import requests
+from pymongo import MongoClient
+
+api_url = "http://127.0.0.1:8000/winners/"
+
+mongo_uri = "mongodb+srv://dblanco:XPi8dTIHmjuk9bH9@cluster0.elpgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+response = requests.get(api_url)
+data = response.json()["winners"]
+
+client = MongoClient(mongo_uri)
+db = client["PremiosOscar"]
+collection = db["Ganadores"]
+
+for entry in data:
+    category = entry["category"]
+    year = entry["year"]
+    nominees = entry["nominees"]
+    movies = entry["movies"]
+    
+    for movie in movies:
+        title = movie["title"]
+        
+        if title in nominees:
+            nominee_str = title
+            document = {
+                "category": category,
+                "year": year,
+                "nominee": nominee_str,
+            }
+        else:
+            nominee_str = ", ".join(nominees)
+            document = {
+                "category": category,
+                "year": year,
+                "nominee": nominee_str,
+                "title": title,
+            }
+
+        if not collection.find_one({"title": title, "nominee": nominee_str}):
+            collection.insert_one(document)
+
+print("Datos cargados correctamente en MongoDB.")
