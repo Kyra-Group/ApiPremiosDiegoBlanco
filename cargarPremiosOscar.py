31b61b03e95869c986e057cd62e6cc0727d4540c
diegoblanco3010 <dblanco@kyramadrid.es>
2024-12-03 13:45:37 +0100

    Update cargarPremiosOscar.py

--- cargarPremiosOscar.py
+++ cargarPremiosOscar.py
@@ -1,1 +1,33 @@
+import requests
+import pymongo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = "https://apipremiosdiegoblanco.onrender.com/api/nominations"
+response = requests.get(url)
+data = response.json()['nominations']
+
+client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = client['PremiosOscar']
+collection = db['Ganadores']
+
+for nomination in data:
+    category = nomination['category']
+    year = nomination['year']
+    nominees = nomination['nominees']
+    movies = nomination['movies']
+
+    for movie in movies:
+        title = movie['title']
+        if title in nominees:
+            nominees = [title]
+
+    document = {
+        "category": category,
+        "year": year,
+        "nominees": nominees
+    }
+    collection.insert_one(document)
 
